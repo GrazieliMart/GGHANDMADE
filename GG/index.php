396a2b383aa154bbef00f7b22e0ce1f5f91ddf5e
Grazieli Martins <143179026+GrazieliMart@users.noreply.github.com>
2023-09-15 17:06:14 -0300

    Add files via upload

---
+++ GG/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Nav.css">
+    <link rel="stylesheet" href="css/Index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
+    
+    <title>GGHANDMADE | Home</title>
+</head>
+<?php
+include 'php/Nav.php';
+?>
+<body>
+   <div class="back-index"> 
+fg
+   </div>
+
+    <script src="js/scriptNav.js"></script>
+</body>
+
+
+</html>
---
+++ GG/php/Nav.php
@@ -0,0 +1,55 @@
+<?php
+echo ' <header>
+<nav class="nav-bar">
+    <div class="logo">
+        <img src="img/logo.jpeg" class="logoNav">
+    </div>
+    <div class="nav-list">
+        <ul>
+            <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Artisan KeyCaps</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Crochet</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Sobre nós</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Contato</a></li>
+        </ul>
+    </div>
+    <div class="login-Icons">
+        
+        <button ><a href="#" id="icon-login"><i class="bi bi-person" ></i></a>
+        <a href="#" id="icon-conta">Conta</a>
+        </button>
+       
+        <button><a href="#" id="icon-bag"><i class="bi bi-bag"></i></a></button>
+        
+    </div>
+
+    <div class="mobile-menu-icon">
+       <button onclick="menuShow()"><img class="icon" src="img/icone-aberto.svg"></button>
+    </div>
+</nav>
+
+
+
+<div class="mobile-menu">
+    <ul>
+    <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+    <li class="nav-item"><a href="#" class="nav-link">Artisan KeyCaps</a></li>
+    <li class="nav-item"><a href="#" class="nav-link">Crochet</a></li>
+    <li class="nav-item"><a href="#" class="nav-link">Sobre nós</a></li>
+    <li class="nav-item"><a href="#" class="nav-link">Contato</a></li>
+    </ul>
+
+    <div class="login-Icons">
+        <button><a href="#" id="icon-login"><i class="bi bi-person" ></i></a>
+        <a href="#" id="icon-conta">Conta</a>
+        </button>
+       
+        <button><a href="#" id="icon-bag"><i class="bi bi-bag"></i></a></button>
+        
+    </div>
+
+    <div class="mobile-menu">
+    
+</div>
+</header>';
+?>
